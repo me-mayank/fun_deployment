372774acf664b9a92fec017378de4ea7608d0cf1
Mayank Tripathi <mayanktripathi@Mayanks-MacBook-Air.local>
2025-08-08 22:12:37 +0530

    Initial commit: backend setup with Express and .gitignore

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config
+const app = express()
+const port = process.env.PORT || 5050
+
+app.get('/', (req, res) => {
+    res.send("The server is operational")
+})
+
+app.get('/home', (req, res) => {
+    res.send('<h1>Welcome to the homepage</h1>')
+})
+
+app.listen(process.env.PORT,() => {
+    console.log(`The server is listening on port ${port}`)
+})
